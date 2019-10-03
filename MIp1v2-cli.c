2682b1e65713ccfc2054d9151e11ca954f928c05
Xarxes user <xarxes@UbuntuX1.(none)>
2019-10-03 20:24:50 +0200

    Funcions del main passades al respectiu .c

--- MIp1v2-cli.c
+++ MIp1v2-cli.c
@@ -18,62 +18,11 @@ int main(int argc,char *argv[])
  /* Es crea el socket TCP scon del client (el socket "local"), que de moment no té       */ 
  /* adreça (@IP i #port TCP) assignada.                                                  */ 
  
- TCP_CreaSockClient("88.8.12.3,TCP,2000",s1);
+ TCP_CreaSockClient("88.8.12.3,TCP,2000");
  TCP_DemanaConnexio(s1,"23.3.1.5,TCP,3678");
  TCP_Envia(s1,"Com va això?");
  TCP_Envia(s1,"Estàs bé?");
  TCP_TancaSock(s1);
  
- if((scon=socket(AF_INET,SOCK_STREAM,0))==-1) 
- { 
-  perror("Error en socket"); 
-  exit(-1); 
- } 
- /* Per assignar adreça a scon (@IP i #port TCP) el programador ho pot fer de 2 maneres: */ 
- /* i) explícitament, és a dir, assignant uns valors concrets en el codi, fent bind()    */ 
- /* amb aquests valors; ii) implícitament, és a dir, deixant que els assigni el S.O.,    */ 
- /* que escull una @IP qualsevol de la màquina i un #port TCP que estigui lliure. Una    */ 
- /* assignació implicita es pot fer de 2 maneres: i) fent bind() amb @IP 0.0.0.0 (que    */ 
- /* vol dir qualsevol, és a dir, totes les @IP de la màquina) i #port TCP 0 (llavors,    */ 
- /* després del bind(), el S.O. haurà assignat un #port TCP, i després del connect() el  */ 
- /* S.O. haurà assignat una @IP, la de la interfície de sortida); ii) no fent bind()     */ 
- /* (llavors, després del connect(), el S.O. haurà assignat un #port TCP i una @IP, la   */ 
- /* de la interfície de sortida).                                                        */ 
- /* Aquí no es fa bind().                                                                */ 
- /* Es connecta scon al socket del servidor (el socket "remot"). Primer s’omple adrrem   */ 
- /* amb l’adreça del socket remot (@IP i #port TCP) i després es fa connect(). A més,    */ 
- /* com que abans no s'ha fet bind(), ara connect() farà que s’assigni una adreça a scon */ 
- /* (@IP i #port TCP) de manera implícita (l'@IP de la interfície de xarxa per on surti  */ 
- /* i un #port TCP qualsevol lliure).                                                    */ 
- strcpy(iprem,"10.0.0.23"); 
- portrem = 3000; 
- adrrem.sin_family=AF_INET; 
- adrrem.sin_port=htons(portrem); 
- adrrem.sin_addr.s_addr= inet_addr(iprem); 
- for(i=0;i<8;i++){adrrem.sin_zero[i]='\0';} 
- if((connect(scon,(struct sockaddr*)&adrrem,sizeof(adrrem)))==-1) 
- { 
-  perror("Error en connect"); 
-  close(scon); 
-  exit(-1); 
- } 
- /* Un cop fet connect() es diu que el socket scon està "connectat" al socket remot.     */ 
- /* Com que és un socket TCP això també vol dir que s'ha establert una connexió TCP.     */ 
- /* S'envia pel socket connectat scon el que es llegeix del teclat                       */ 
- if((bytes_llegits=read(0,buffer,sizeof(buffer)))==-1) 
- { 
-	   perror("Error en read"); 
-  close(scon); 
-  exit(-1); 
- } 
- if((bytes_escrits=write(scon,buffer,bytes_llegits))==-1) 
- { 
-  perror("Error en write"); 
-  close(scon); 
-  exit(-1); 
- } 
- /* Es tanca el socket scon, que com que és un socket TCP, també vol dir que es tanca la */ 
- /* connexió TCP establerta.                                                             */ 
- close(scon); 
  return(0); 
 } 
--- MIp1v2-ser.c
+++ MIp1v2-ser.c
@@ -16,87 +16,12 @@ int main(int argc,char *argv[])
  char iploc[16]; 
  int portloc; 
  int s2=0;
- /* Es crea el socket TCP sesc del servidor (el socket "local"), que de moment no té     */ 
- /* adreça (@IP i #port TCP) assignada.                                                  */ 
+                                                */ 
  TCP_CreaSockServidor("23.3.1.5,TCP,3678");
  TCP_AcceptaConnexio(s2,"88.8.12.3,TCP,2000");
  TCP_Rep(s2,"Com va això?");
  TCP_Rep(s2,"Estàs bé?");
  TCP_TancaSock(s2);
  
-if((sesc=socket(AF_INET,SOCK_STREAM,0))==-1)
- { 
-  perror("Error en socket"); 
-  exit(-1); 
- } 
- /* Per assignar adreça a sesc (@IP i #port TCP) el programador ho pot fer de 2 maneres: */ 
- /* i) explícitament, és a dir, assignant uns valors concrets en el codi, fent bind()    */ 
- /* amb aquests valors; ii) implícitament, és a dir, deixant que els assigni el S.O.,    */ 
- /* que escull una @IP qualsevol de la màquina i un #port TCP que estigui lliure. Una    */ 
- /* assignació implicita es pot fer d'1 manera: fent bind() amb @IP 0.0.0.0 (que vol dir */ 
- /* qualsevol, és a dir, totes les @IP de la màquina) i #port TCP 0 (llavors, després    */ 
- /* del bind(), el S.O. haurà assignat un #port TCP, i després del accept() el S.O.      */ 
- /* haurà assignat una @IP, la 
-de la interfície d'entrada). Assignar l'@IP 0.0.0.0 vol   */ 
- /* dir que el socket queda lligat a totes les @IP de la màquina i que escoltarà         */ 
- /* peticions de connexió que arribin a qualsevol d'elles.                               */ 
- /* Aquí es fa bind amb @IP 0.0.0.0 i #port TCP 3000.                                    */ 
- strcpy(iploc,"0.0.0.0");    /* 0.0.0.0 correspon a INADDR_ANY */ 
- portloc = 3000; 
- adrloc.sin_family=AF_INET; 
- adrloc.sin_port=htons(portloc); 
- adrloc.sin_addr.s_addr=inet_addr(iploc);    /* o bé: ...s_addr = INADDR_ANY */ 
- for(i=0;i<8;i++){adrloc.sin_zero[i]='\0';} 
- if((bind(sesc,(struct sockaddr*)&adrloc,sizeof(adrloc)))==-1) 
- { 
-  perror("Error en bind"); 
-  close(sesc); 
-  exit(-1); 
- } 
-/* Es crea una cua per emmagatzemar peticions de connexió pendents. 
-                     */ 
- if((listen(sesc,3))==-1) 
- { 
-  perror("Error en listen"); 
-  close(sesc); 
-  exit(-1); 
- } 
- /* Un cop fet listen() es diu que sesc és un socket "d'escolta".
-                        */ 
- /* Ara espera una 
-petició de connexió TCP d’un client, i un cop rebuda, l'accepta. El   */ 
- /* descriptor de socket retornat, scon, serà un nou identificador del socket local per  */ 
- /* a la connexió que s'acaba d'establir. Ambdós sockets tenen la mateixa adreça (@IP i  */
- /* #port TCP), però sesc és un socket “d’escolta” i scon és un socket "connectat". En   */ 
- /* aquest cas, però, com que sesc no tenia assignada una @IP concreta, sinó qualsevol,  */ 
- /* és a dir, totes les de la màquina (l'@IP 0.0.0.0), scon tindrà la “mateixa” @IP,     */ 
- /* però “concretada” a una d’elles (el S.O. li assignarà l’@IP que hagi fet servir el   */ 
- /* client en la petició de connexió).                                                   */ 
- long_adrrem=sizeof(adrrem); 
- if((scon=accept(sesc,(struct sockaddr*)&adrrem, &long_adrrem))==-1) 
- { 
-  perror("Error en accept"); 
-  close(sesc); 
-  exit(-1); 
- } 
- /* Ara 
-adrrem conté l'adreça del socket remot (@IP i #port TCP).                        */ 
- /* S'escriu a pantalla el que arriba pel socket connectat scon                          */ 
- if((bytes_llegits=read(scon,buffer,sizeof(buffer)))==-1) 
- { 
-  perror("Error en read"); 
-  close(scon); 
-  exit(-1); 
- } 
- if((bytes_escrits=write(1,buffer,bytes_llegits))==-1) 
- { 
-  perror("Error en write"); 
-  close(scon); 
-  exit(-1); 
- } 
- /* Es tanca el socket scon, que com que és un socket TCP, també vol dir que es tanca la */  
-  /* connexió TCP establerta, i es tanca el socket d'escolta sesc.                        */ 
- close(scon); 
- close(sesc); 
  return(0); 
 } 
